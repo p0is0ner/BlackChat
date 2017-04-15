1f7e8c9e4df7dfb9a04be298bd3144ba9da131b3
poison <backtrack.adz@gmail.com>
2017-04-15 22:32:02 +0200

    Update adapter.py

--- modules/adapter.py
+++ modules/adapter.py
@@ -4,7 +4,7 @@ It executes the queries and return the results, by this way
 the server script only needs to treat them.
 """
 
-import sqlite3  # database releated stuff like cursor and connection
+import sqlite3  # database related stuff like cursor and connection
 from queries import Container  # queries files
 
 container = Container()  # making a local instance of the needed class
@@ -22,6 +22,21 @@ class Adapter:
         # deleting the table, run this query in the server shell,
         #  the server will abort and delete all files related to it
         self.delete_table = container.delete_table
+        # variable containing database name
+        self.database = 'blackchat.db'
+        # instance of the conn
+        self.conn = sqlite3.connect(self.database)
+        # instance of the cursor
+        self.cursor = self.conn.cursor()
+        
+    def close_conn(self):
+        """
+        this function will be called in all others to close the connection after the
+        execution of the query
+        :return: 
+        """
+        self.cursor.close()
+        self.conn.close()
 
     def create_table(self):
         """
@@ -29,16 +44,16 @@ class Adapter:
         the table will be created if it doesnt already exist
         :return: True or False values 
         """
-        conn = sqlite3.connect('blackchat.db')
+        conn = sqlite3.connect(self.database)
         cursor = conn.cursor()
 
         try:
             cursor.execute(self.create_table)
             conn.commit()
-            cursor.close()
-            conn.close()
+            self.close_conn()
             return True
         except sqlite3.OperationalError:
+            self.close_conn()
             return False
 
     def create_account(self, username, password, ip):
@@ -47,16 +62,16 @@ class Adapter:
         user entered the required command in the client shell 
         :return: True or False values
         """
-        conn = sqlite3.connect('blackchat.db')
+        conn = sqlite3.connect(self.database)
         cursor = conn.cursor()
 
         try:
             cursor.execute(self.create_account, (username, password, ip))
             conn.commit()
-            cursor.close()
-            conn.close()
+            self.close_conn()
             return True
         except sqlite3.OperationalError:
+            self.close_conn()
             return False
 
     def delete_account(self, username):
@@ -65,16 +80,16 @@ class Adapter:
         :param username: 
         :return: 
         """
-        conn = sqlite3.connect('blackchat.db')
+        conn = sqlite3.connect(self.database)
         cursor = conn.cursor()
 
         try:
             cursor.execute(self.delete_account, (username, ))
             conn.commit()
-            cursor.close()
-            conn.close()
+            self.close_conn()
             return True
         except sqlite3.OperationalError:
+            self.close_conn()
             return False
 
     def delete_table(self):
@@ -86,14 +101,14 @@ class Adapter:
         server will abort too
         :return: 
         """
-        conn = sqlite3.connect('blackchat.db')
+        conn = sqlite3.connect(self.database)
         cursor = conn.cursor()
 
         try:
             cursor.execute(self.delete_table)
             conn.commit()
-            cursor.close()
-            conn.close()
+            self.close_conn()
             return True
         except sqlite3.OperationalError:
+            self.close_conn()
             return False
